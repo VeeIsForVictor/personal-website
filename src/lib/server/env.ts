e6bc12d534295c99cc0d86a14394ae8a93655e35
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-06-16 04:56:54 +0800

    feat: add flowbite component library

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,5 +1,5 @@
-import assert from "assert";
-import { env } from "$env/dynamic/private";
+import assert from 'assert';
+import { env } from '$env/dynamic/private';
 
 assert(typeof env.PUBLIC_APIURL !== 'undefined');
-export const { PUBLIC_APIURL } = env
+export const { PUBLIC_APIURL } = env;
