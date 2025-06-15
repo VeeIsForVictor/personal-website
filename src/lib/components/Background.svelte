e1bebdd1fa4635c77f6b1f9bbe6d7dafc1c4e177
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-06-16 05:16:43 +0800

    feat: add Background

---
+++ src/lib/components/Background.svelte
@@ -0,0 +1,131 @@
+<div class="background p-10 flex flex-col">
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</div>
+
+<style>
+	@keyframes animate {
+		0% {
+			transform: translateY(0) rotate(0deg);
+			opacity: 1;
+			border-radius: 0;
+		}
+		100% {
+			transform: translateY(-1000px) rotate(720deg);
+			opacity: 0;
+			border-radius: 50%;
+		}
+	}
+
+	.background {
+		position: fixed;
+		width: 100vw;
+		height: 100vh;
+		top: 0;
+		left: 0;
+		margin: 0;
+		padding: 0;
+		z-index: -10;
+		overflow: hidden;
+
+		background: var(--color-primary-500);
+
+		@media (prefers-color-scheme: dark) {
+			background: var(--color-primary-900);
+		}
+	}
+	
+	/* Dark mode support using a .dark class on a parent element */
+
+	.background li {
+		position: absolute;
+		display: block;
+		list-style: none;
+		width: 20px;
+		height: 20px;
+		background: rgba(255, 255, 255, 0.2);
+		animation: animate 25s linear infinite;
+	}
+
+	.background li:nth-child(0) {
+		left: 22%;
+		width: 177px;
+		height: 177px;
+		bottom: -177px;
+		animation-delay: 1s;
+	}
+	.background li:nth-child(1) {
+		left: 89%;
+		width: 187px;
+		height: 187px;
+		bottom: -187px;
+		animation-delay: 4s;
+	}
+	.background li:nth-child(2) {
+		left: 55%;
+		width: 154px;
+		height: 154px;
+		bottom: -154px;
+		animation-delay: 10s;
+	}
+	.background li:nth-child(3) {
+		left: 71%;
+		width: 145px;
+		height: 145px;
+		bottom: -145px;
+		animation-delay: 15s;
+	}
+	.background li:nth-child(4) {
+		left: 26%;
+		width: 189px;
+		height: 189px;
+		bottom: -189px;
+		animation-delay: 14s;
+	}
+	.background li:nth-child(5) {
+		left: 61%;
+		width: 140px;
+		height: 140px;
+		bottom: -140px;
+		animation-delay: 21s;
+	}
+	.background li:nth-child(6) {
+		left: 72%;
+		width: 131px;
+		height: 131px;
+		bottom: -131px;
+		animation-delay: 21s;
+	}
+	.background li:nth-child(7) {
+		left: 65%;
+		width: 189px;
+		height: 189px;
+		bottom: -189px;
+		animation-delay: 25s;
+	}
+	.background li:nth-child(8) {
+		left: 58%;
+		width: 145px;
+		height: 145px;
+		bottom: -145px;
+		animation-delay: 36s;
+	}
+	.background li:nth-child(9) {
+		left: 58%;
+		width: 127px;
+		height: 127px;
+		bottom: -127px;
+		animation-delay: 33s;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+    import Background from '$lib/components/Background.svelte';
     import '../app.css';
 
     const { children } = $props();
 </script>
+
+<Background />
 
 {@render children()}
