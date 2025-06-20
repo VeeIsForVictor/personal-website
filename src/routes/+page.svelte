04422ba5a32e4989cf0fb4874e324a6989f8c472
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-06-20 16:04:47 +0800

    feat: add homepage blurb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="prose dark:prose-invert">
+    <h1>
+        Hello, world!
+    </h1>
+    
+    <h3>
+        I'm Victor and this is the *second* iteration of my website 💀
+    </h3>
+</div>
