4975bbe7cfd412b7ea33c1d70ae30be5c7eff010
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-08-21 18:54:55 +0800

    feat: add navbar to layout

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { NavigationMenu } from 'bits-ui';
+</script>
+
+<NavigationMenu.Root class="w-full p-4 bg-white shadow-md rounded-lg">
+    <NavigationMenu.List class="flex w-1/2 justify-around mx-auto">
+        <NavigationMenu.Item>
+            <NavigationMenu.Link href="/">Home</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+            <NavigationMenu.Link href="/work">Work</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+            <NavigationMenu.Link href="/blog">Blog</NavigationMenu.Link>
+        </NavigationMenu.Item>
+    </NavigationMenu.List>
+</NavigationMenu.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import '../app.css';
+    import NavBar from '$lib/components/NavBar.svelte';
 
     const { children } = $props();
 </script>
 
-<div class="w-dvw">
-    <main class="flex flex-col items-center justify-center h-screen">
+<div class="w-dvw flex flex-col items-center h-screen">
+    <NavBar />
+
+    <main class="flex flex-col items-center justify-center h-full">
         {@render children()}
     </main>
 </div>
