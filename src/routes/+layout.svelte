67c5f85c13a102ea02e9dd11c34cf91f7b4a7923
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-08-21 13:19:10 +0800

    feat: set up basic spacing in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
     const { children } = $props();
 </script>
 
-{@render children()}
+<div class="w-dvw">
+    <main class="flex flex-col items-center justify-center h-screen">
+        {@render children()}
+    </main>
+</div>
+
