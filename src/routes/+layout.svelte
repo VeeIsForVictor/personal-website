360f70407931945a2f2afe18b8b7c36aea22309e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-10-05 22:39:43 +0800

    feat: control max width in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <div class="flex h-screen w-dvw flex-col items-center bg-primary-900 dark:text-white">
     <NavBar />
 
-    <main class="flex h-full flex-col items-center justify-center max-w-6xl px-4 mx-4 rounded-lg shadow-lg overflow-auto">
+    <main class="flex h-full flex-col items-center justify-center max-w-6xl min-w-2xl px-4 mx-4 rounded-lg shadow-lg overflow-auto">
         {@render children()}
     </main>
 
