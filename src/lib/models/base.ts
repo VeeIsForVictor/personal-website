d2b5d7b3f186146ba84bdf480277f8da667b890a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-11 22:57:21 +0800

    fix: allow date created in base schema to be optional

--- src/lib/models/base.ts
+++ src/lib/models/base.ts
@@ -4,7 +4,7 @@ import * as v from 'valibot';
 export function buildBaseSchema(idSchema: v.BaseSchema<any, any, v.BaseIssue<unknown>>) {
     return v.object({
         id: idSchema,
-        date_created: v.string(),
+        date_created: v.optional(v.string()),
         date_updated: v.nullable(v.string()),
     });
 }
