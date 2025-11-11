5e01048b76dad544ab1f4c7790dde25e781a2774
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-12 01:09:06 +0800

    feat: add home block elements to schema

--- src/lib/models/schema.ts
+++ src/lib/models/schema.ts
@@ -1,7 +1,11 @@
 import { Article } from './article';
 import { Home } from './home';
+import type { HBlockCardgroup, HBlockHero, HomepageElementsData } from './homepage_elements';
 
 export interface Schema {
     articles: Article[];
-    home: Home
+    home: Home;
+    home_homepage_elements: HomepageElementsData;
+    HBlock_hero: HBlockHero;
+    HBlock_cardgroup: HBlockCardgroup;
 }
