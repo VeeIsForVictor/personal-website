44b49ecb39cc1f574761412189add6427648ad73
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-12 15:07:22 +0800

    feat: set up hero component

---
+++ src/lib/components/home/Hero.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { HBlockHero } from "$lib/models/homepage_elements";
+
+    interface Props {
+        heroBlockData: HBlockHero; 
+    }
+
+    const { heroBlockData }: Props = $props();
+</script>
