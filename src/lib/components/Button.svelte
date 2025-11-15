1e42fb07e7aa92037e8d122f475a307ee47ba971
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-15 13:00:20 +0800

    feat: implement buttons

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Button } from "bits-ui";
+    import type { Snippet } from "svelte";
+
+    interface Props {
+        href?: string
+        children?: Snippet
+    }
+
+    const { href, children }: Props = $props();
+</script>
+
+<Button.Root {href}>
+    {@render children?.()}
+</Button.Root>
--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
     import type { HBlockHero } from "$lib/models/homepage_elements";
+    import Button from "../Button.svelte";
 
     interface Props {
         heroBlockData: HBlockHero; 
     }
 
     const { heroBlockData }: Props = $props();
+    const { buttons, image } = $derived(heroBlockData);
 </script>
 
 <div class="flex flex-row prose dark:prose-invert">
     <div class="flex flex-col">
         <h1>{heroBlockData.headline}</h1>
         <div>{@html heroBlockData.content}</div>
+        {#if buttons !== null && buttons.length > 0}
+            <div>
+                {#each buttons as {label, href}}
+                    <Button {href}>
+                        {label}
+                    </Button>
+                {/each}
+            </div>
+        {/if}
     </div>
-    {#if heroBlockData.image !== null}
+    {#if image !== null}
+    
         <div>
-            <img src={heroBlockData.image.id} alt={heroBlockData.image.description}>
+            <img src={image.id} alt={image.description}>
         </div>
     {/if}
 </div>
