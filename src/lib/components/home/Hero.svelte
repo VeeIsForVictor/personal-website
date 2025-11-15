be2fa04cb23857c99458c11bf9335da7d77a82b4
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-15 23:30:28 +0800

    fix: correct spacing in hero

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -10,9 +10,9 @@
     const { buttons, image } = $derived(heroBlockData);
 </script>
 
-<div class="flex flex-col lg:flex-row-reverse w-full">
+<div class="flex flex-col lg:flex-row-reverse w-full my-4">
     {#if image !== null}
-        <div class="w-1/3 mx-auto overflow-visible mt-4 transform translate-y-8 z-10">
+        <div class="w-1/3 mx-auto overflow-visible transform translate-y-8 z-10">
             <img
                 class="aspect-square rounded-2xl shadow-sm w-full h-auto object-cover border border-primary-800"
                 src={image.id}
@@ -20,7 +20,7 @@
             />
         </div>
     {/if}
-    <div class="flex flex-col p-6 pb-8 pt-10 border border-primary-800 rounded-2xl prose dark:prose-invert">
+    <div class="flex flex-col p-6 pb-8 pt-10 border border-primary-800 rounded-2xl prose dark:prose-invert prose-h1:mb-0">
         <h1>{heroBlockData.headline}</h1>
         <div>{@html heroBlockData.content}</div>
         {#if buttons !== null && buttons.length > 0}
