c0df8265bd3faba77ccd409449ffa9e28b5c1b5b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-15 14:56:08 +0800

    feat: set layout styling for hero component

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -10,7 +10,16 @@
     const { buttons, image } = $derived(heroBlockData);
 </script>
 
-<div class="flex flex-row prose dark:prose-invert">
+<div class="flex flex-col lg:flex-row-reverse prose dark:prose-invert w-full">
+    {#if image !== null}
+        <div class="w-1/4 m-auto overflow-clip">
+            <img 
+                class="aspect-square"
+                src={image.id} 
+                alt={image.description}
+            >
+        </div>
+    {/if}
     <div class="flex flex-col">
         <h1>{heroBlockData.headline}</h1>
         <div>{@html heroBlockData.content}</div>
@@ -24,10 +33,4 @@
             </div>
         {/if}
     </div>
-    {#if image !== null}
-    
-        <div>
-            <img src={image.id} alt={image.description}>
-        </div>
-    {/if}
 </div>
