1ac6808791d9c4510a61369709b6e1c9dea0606c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-16 00:36:28 +0800

    fix: ensure vertical centering of first element when sole

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
 <div class="flex grow flex-col items-center gap-4">
     {#each homepageElements as element, idx}
         {#if idx == 0}
-            <div class="from-primary-950 to-primary-900 w-screen bg-linear-to-b pt-12">
-                <div class="mx-auto flex w-5/6">
+            <div class="flex flex-col grow items-center from-primary-950 to-primary-900 w-screen bg-linear-to-b pt-12">
+                <div class="flex my-auto self-center w-5/6">
                     {#if element.collection === 'HBlock_hero'}
                         <Hero heroBlockData={element.item} />
                     {/if}
