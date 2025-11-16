a08e0b71fab2e62c6c04386ab75f9801dd053ad7
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-16 12:05:02 +0800

    feat: add slots for cardgroup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import CardGroup from '$lib/components/home/CardGroup.svelte';
     import Hero from '$lib/components/home/Hero.svelte';
 
     const { data } = $props();
@@ -7,18 +8,23 @@
 
 <div class="flex grow flex-col items-center gap-4">
     {#each homepageElements as element, idx}
+        {@const { collection, item } = element}
         {#if idx == 0}
             <div class="flex flex-col grow items-center from-primary-950 to-primary-900 w-screen bg-linear-to-b pt-12">
                 <div class="flex my-auto self-center w-5/6">
-                    {#if element.collection === 'HBlock_hero'}
-                        <Hero heroBlockData={element.item} />
-                    {/if}
-                </div>
+                        {#if collection === 'HBlock_hero'}
+                            <Hero heroBlockData={item} />
+                        {:else if collection === 'HBlock_cardgroup'}
+                            <CardGroup cardgroupData={item} {promisedArticles} />
+                        {/if}
+                    </div>
             </div>
         {:else}
             <div class="mx-auto flex w-5/6 last:pb-12">
-                {#if element.collection === 'HBlock_hero'}
-                    <Hero heroBlockData={element.item} />
+                {#if collection === 'HBlock_hero'}
+                    <Hero heroBlockData={item} />
+                {:else if collection === 'HBlock_cardgroup'}
+                    <CardGroup cardgroupData={item} {promisedArticles} />
                 {/if}
             </div>
         {/if}
