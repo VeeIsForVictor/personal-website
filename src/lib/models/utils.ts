e03857d7767b58c53878bc94225cb7aabaecd939
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-16 23:10:08 +0800

    fix: move environment variable import

--- src/lib/models/utils.ts
+++ src/lib/models/utils.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_APIURL } from '$env/static/public';
+import { PUBLIC_APIURL } from '$lib/server/env';
 import * as v from 'valibot';
 
 export const imageIdToUrl = v.transform((imageId: string) => `${PUBLIC_APIURL}/assets/${imageId}`);
