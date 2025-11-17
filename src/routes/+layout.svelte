c75729e45815d79d37be9507fe698fc9347454ec
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-11-17 14:21:05 +0800

    feat: set og meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,12 @@
     <title>{title}</title>
     <meta charset="utf-8" />
     <meta name="description" content={description} />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://veeisforvictor.work" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content="%sveltekit.assets%/favicons/android-chrome-192x192.png" />
 </svelte:head>
 
 <div
